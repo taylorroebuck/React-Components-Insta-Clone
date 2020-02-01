0d4fc0a292b9d12977cdb8b339fa79052f5c8dcd
TN Roebuck <tnroebuck@TNs-MacBook-Pro.local>
2020-01-31 20:23:08 -0700

    added imports to app.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import PostsPage from "./components/PostsContainer/PostsPage";
+import SearchBar from "./components/SearchBar/SearchBarContainer";
+import dummydata from "./dummy-data";
+
 // import the PostsPage and SearchBar and add them to the App
 // import dummydata
 
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,7 +4,8 @@ import Post from "./Post";
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = (props) => {
+  console.log("postsPage props", props);
   return (
     <div className="posts-container-wrapper">
       {/* map through data here */}
